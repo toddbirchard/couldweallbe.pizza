038669bcbd9f6217bff61b17f1a76dd182747abc
toddbirchard <toddbirchard@gmail.com>
2022-01-07 14:55:10 -0500

    Add link to author pages on PostCards.

--- src/components/common/PostCard.js
+++ src/components/common/PostCard.js
@@ -7,16 +7,19 @@ import { readingTime as readingTimeHelper } from '@tryghost/helpers'
 import '../../styles/post-card.less'
 
 const PostCard = ({ post }) => {
+    const author = post.primary_author
     const url = `/${post.slug}/`
     const readingTime = readingTimeHelper(post)
+    const authorImage = author.profile_image
+    const authorName = author.name
+    const authorUrl = post.primary_author.slug && `/author/${post.primary_author.slug}/`
 
     return (
         <Link to={url} className="post-card">
 
             {post.feature_image &&
-                    <div className="post-card-image" style={{
-                        backgroundImage: `url(${post.feature_image})` ,
-                    }}></div>}
+              <div className="post-card-image" style={{backgroundImage: `url(${post.feature_image})`}}></div>
+            }
 
             <div className="post-card-body">
                 <header className="post-card-header">
@@ -28,15 +31,17 @@ const PostCard = ({ post }) => {
                 <footer className="post-card-footer">
                     <div className="post-card-footer-left">
                         <div className="post-card-avatar">
-                            {post.primary_author.profile_image ?
-                                <img className="author-profile-image" src={post.primary_author.profile_image} alt={post.primary_author.name}/> :
-                                <img className="default-avatar" src="/images/icons/avatar.svg" alt={post.primary_author.name}/>
+                          <Link to={authorUrl}>
+                            {authorImage ?
+                                <img className="author-profile-image" src={authorImage} alt={authorName}/> :
+                                <img className="default-avatar" src="/images/icons/avatar.svg" alt={authorName}/>
                             }
+                          </Link>
                         </div>
-                        <Link className="author-name" to={`/author/${post.primary_author.slug}`}>{ post.primary_author.name }</Link>
-                    </div>
-                    <div className="post-card-footer-right">
-                        <div>{readingTime}</div>
+                        <div>
+                          <Link className="author-name" to={authorUrl}>{authorName}</Link>
+                          <div className={"reading-time"}>{readingTime}</div>
+                        </div>
                     </div>
                 </footer>
             </div>
